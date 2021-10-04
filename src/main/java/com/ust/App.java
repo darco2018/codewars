cbf4eea9bcd2549618b61aaf4a4f8616563de23d
darco2018 <newsecond@outlook.com>
2021-10-04 14:37:32 +0200

    - - - change pom to java 11

--- src/main/java/com/ust/App.java
+++ src/main/java/com/ust/App.java
@@ -1,13 +1,18 @@
 package com.ust;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!Claspath is " + System.getProperty("java.class.path") );
+        String multilineString = "Baeldung helps \n \n developers \n explore Java.";
+        List<String> lines = multilineString.lines()
+                .filter(line -> !line.isBlank())
+                .map(String::strip)
+                .collect(Collectors.toList());
+        System.out.println( lines);
     }
 }
